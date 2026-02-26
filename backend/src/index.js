f352970b2a51a9c6566e9af7b1daca730b39dc2a
Sweety <rsweet375@gmail.com>
2026-02-26 17:14:08 +0530

    update profile endpoint added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
@@ -8,6 +9,7 @@ import { ENV } from "./lib/env.js";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 const __dirname = path.resolve();
 
 app.use("/api/auth", authRoutes);
