b4ed4b613d2d4dd79f509be6fe2cee2d578272e9
Sweety <rsweet375@gmail.com>
2026-03-13 17:25:42 +0530

    add arcjet protection

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 // the middlewares execute in order - so requests get rate-limited first, then authenticated.
 // this is actually more efficient since unauthenticated requests get blocked by rate limiting before hitting the auth middleware.
-router.use(protectRoute);
+router.use(arcjetProtection, protectRoute);
 
 router.get("/contacts", getAllContacts);
 router.get("/chats", getChatPartners);
